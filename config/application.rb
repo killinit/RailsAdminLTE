568794a49369a7682cb09213954b24d01410bc47
pranavpr <pranav.iitkgp@gmail.com>
2014-09-07 23:47:38 +0530

    Modify production.rb

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,6 @@ module Adminlte
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << Rails.root.join("vendor","assets", "fonts")
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.svg *.eot *.woff *.ttf )
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.svg *.eot *.woff *.ttf)
   end
 end
