e2fabcf0cdfc070f60a47175211f05b6495b2f93
pranavpr <pranav.iitkgp@gmail.com>
2014-09-10 01:02:03 +0530

    Cleanup assets for Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Adminlte::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
   config.assets.compress = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
