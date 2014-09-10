f27a897f50a8b0e414b8caba63a7aa90d368f384
Pranav <pranav@pranavprakash.net>
2014-09-10 21:00:38 +0530

    Fix gzip asset serving for Heroku

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module Adminlte
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.middleware.use Rack::Deflater
     config.assets.paths << Rails.root.join("vendor","assets", "fonts")
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.svg *.eot *.woff *.ttf)
   end
