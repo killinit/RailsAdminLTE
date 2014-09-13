d8435079f4201c84f2e0ee7ba4a38d7e9e3593ec
pranavpr <pranav@pranavprakash.net>
2014-09-14 01:08:06 +0530

    Create Log-in page UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Adminlte::Application.routes.draw do
-  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
+  resources :sessions, only: [:new, :create, :destroy]
+  root "sessions#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
