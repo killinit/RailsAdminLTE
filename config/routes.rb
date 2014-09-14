0a327a8d4dd081a6518c2e3d94fd09642a535e35
pranavpr <pranav@pranavprakash.net>
2014-09-14 17:34:22 +0530

    Refractor Login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Adminlte::Application.routes.draw do
-  get "sessions/create"
-  get "sessions/destroy"
   resources :sessions, only: [:new, :create, :destroy]
   root "sessions#new"
+  get "sessions/create"
+  get "sessions/destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
